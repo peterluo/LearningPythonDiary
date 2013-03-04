a18937997f5bcc24c7ae142264cca305a9db06d7
peterluo <peterluo2010@gmail.com>
2013-03-05 00:17:50 +0800

    First commit

---
+++ Hello World.py
@@ -0,0 +1,9 @@
+Python 3.2.3 (default, Apr 11 2012, 07:15:24) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print 'Hello world!'
+SyntaxError: invalid syntax
+>>> print "Hello world!"
+SyntaxError: invalid syntax
+>>> print ("Hello world!")
+Hello world!
+>>> 
