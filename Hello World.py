88a412f839e65a05af914cfced41e7d268d99b98
peterluo <peterluo2010@gmail.com>
2013-03-05 23:53:56 +0800

    2 day

--- Hello World.py
+++ Hello World.py
@@ -1,9 +1,5 @@
-Python 3.2.3 (default, Apr 11 2012, 07:15:24) [MSC v.1500 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print 'Hello world!'
-SyntaxError: invalid syntax
->>> print "Hello world!"
-SyntaxError: invalid syntax
->>> print ("Hello world!")
-Hello world!
->>> 
+
+if __name__ == "__main__":
+ print "hello word"
+
+	
